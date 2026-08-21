04270238d476494fb79243ea289e4615aae0f7be
Vansh <vanshsingh3711@gmail.com>
2026-08-21 17:06:23 +0530

    feat: implement Supabase SSR clients and authentication middleware for session management

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -7,8 +7,8 @@ export async function updateSession(request: NextRequest) {
   })
 
   const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY as string,
     {
       cookies: {
         getAll() {
